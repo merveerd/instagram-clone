8db29fb53f7ad3f7a7536a85522c1002865c5cc9
Merve Erdogmus <merve.erdogmus@adcolony.com>
2020-08-19 23:01:41 +0300

    Initial commit

---
+++ instagramClone/App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+} from 'react-native';
+
+
+
+export default App;
